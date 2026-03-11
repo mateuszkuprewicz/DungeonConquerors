d92ed6d8e810ad2bda438ab1dae62cceabf50277
Mateusz <mateuszkuprewicz8@gmail.com>
2026-03-11 01:53:46 +0100

    Mapa

---
+++ ConsoleApp1/GameMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class GameMap
+    {
+        public const int MapHeight = 20;
+        public const int MapWidth = 40;
+        public Stack<Item>[,] map;
+        public GameMap(string mapFilePath)
+        {
+            using StreamReader reader = new StreamReader(mapFilePath);
+            //if cell is wall, Stack is null
+            map = new Stack<Item>[MapHeight, MapWidth];
+            char c;
+            for(int i = 0; i < MapHeight; i++)
+            {
+                for(int j = 0; j < MapWidth; j++)
+                {
+                    c = (char)reader.Read();
+                    if (c == ' ')
+                    {
+                        map[i, j] = new Stack<Item>();
+                    }
+                }
+                reader.ReadLine();
+            }
+        }
+    }
+}
